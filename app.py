0002ccadd7e5767f45ff77b53306cf63b2db1ce7
deepankarvarma <satwikdpshrit@gmail.com>
2023-04-08 23:30:49 +0530

    added song

--- app.py
+++ app.py
@@ -1,47 +1,75 @@
 import streamlit as st
 import time
+from IPython.display import HTML
 
-# Set default timer values
-pomodoro_time = 25
-short_break_time = 5
-long_break_time = 15
+def pomodoro_timer(pomodoro_length, short_break_length, long_break_length, long_break_interval):
+    
+    pomodoro_count = 0
+    running = False
+    start_button = st.button("Start Pomodoro Timer")
 
-# Define function for Pomodoro timer
-def pomodoro_timer(minutes):
-    st.write(f"Time remaining: {minutes} minutes")
-    seconds = minutes * 60
-    while seconds:
-        mins, secs = divmod(seconds, 60)
-        timer = f"{mins:02d}:{secs:02d}"
-        st.write(timer)
+    reset_button = st.button("Reset Timer") # create a reset button
+
+    if start_button:
+        running = True
+        while running:
+            # Pomodoro session
+            st.write(f"Pomodoro Session #{pomodoro_count + 1}")
+            timer_length = countdown(pomodoro_length, "Pomodoro Duration :- ")
+
+            # Increment pomodoro count and check if long break is needed
+            pomodoro_count += 1
+            if pomodoro_count % long_break_interval == 0:
+                st.write("Time for a long break!")
+                timer_length = countdown(long_break_length, "Long Break Duration :- ")
+            else:
+                st.write("Time for a short break!")
+                timer_length = countdown(short_break_length, "Short Break Duration :- ")
+
+            # Check if reset button is clicked
+            if reset_button:
+                running = False
+                timer_text.empty()
+
+def countdown(timer_length, title):
+    timer_length *= 60
+    while timer_length:
+        mins, secs = divmod(timer_length, 60)
+        timeformat = '{:02d}:{:02d}'.format(mins, secs)
+        timer_text.write(f"{title}{timeformat}") # update the text field with the current time and title
         time.sleep(1)
-        seconds -= 1
-    st.write("Time's up!")
+        timer_length -= 1
+    st.balloons()
 
-# Define Streamlit app
-def main():
-    st.title("Pomodoro Timer")
-    st.write("Select the timer duration below:")
+# Streamlit app code
+st.title("Pomodoro Timer")
+audio_url = st.text_input("Enter MP4 file URL for background music:")
+if audio_url:
+    html = f"""
+        <audio autoplay loop controls>
+            <source src="{audio_url}" type="audio/mp4">
+        </audio>
+    """
+    st.write(HTML(html))
+st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background-image: url("https://cdn.pixabay.com/photo/2019/04/24/11/27/flowers-4151900_960_720.jpg");
+             background-attachment: fixed;
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
+st.write("Welcome to the Pomodoro Timer! Please enter the following values:")
 
-    # Get user input for timer durations
-    pomodoro_time = st.slider("Pomodoro time (minutes)", 1, 60, 25, 1)
-    short_break_time = st.slider("Short break time (minutes)", 1, 60, 5, 1)
-    long_break_time = st.slider("Long break time (minutes)", 1, 60, 15, 1)
+pomodoro_length = st.number_input("Pomodoro Length (minutes)", min_value=1, step=1, value=25)
+short_break_length = st.number_input("Short Break Length (minutes)", min_value=1, step=1, value=5)
+long_break_length = st.number_input("Long Break Length (minutes)", min_value=1, step=1, value=15)
+long_break_interval = st.number_input("Long Break Interval (Pomodoros)", min_value=1, step=1, value=4)
 
-    # Define button for starting Pomodoro timer
-    if st.button("Start Pomodoro"):
-        pomodoro_timer(pomodoro_time)
-        st.write("Take a short break!")
-        time.sleep(short_break_time * 60)
-        st.write("Time for another Pomodoro!")
-        pomodoro_timer(pomodoro_time)
-        st.write("Take a short break!")
-        time.sleep(short_break_time * 60)
-        st.write("Time for another Pomodoro!")
-        pomodoro_timer(pomodoro_time)
-        st.write("Take a long break!")
-        time.sleep(long_break_time * 60)
-        st.write("Time to start again!")
+timer_text = st.empty() # create an empty text field to display the timer
 
-if __name__ == "__main__":
-    main()
+pomodoro_timer(pomodoro_length, short_break_length, long_break_length, long_break_interval)
